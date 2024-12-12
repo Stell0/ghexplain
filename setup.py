837057827a0e596edfe6099fe52d826c11c2a509
Stefano Fancello <gentoo.stefano@gmail.com>
2024-12-12 14:27:48 +0100

    First import

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="ghexplain",
+	packages=find_packages(),
+	install_requires=[
+		"langchain>=0.3.0",
+		"langchain-openai>=0.2.0",
+		"requests>=2.32.3",
+		"python-dotenv>=1.0.0",
+	],
+)
